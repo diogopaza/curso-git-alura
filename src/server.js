1bd5d9070cefed6f2a946d995c60aaae7c6146cd
Diogo Paza <paza@educabiz.com>
2025-12-19 16:53:48 -0300

    conflito

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log('Boas-vindas a API do ALLBOOKS')
+  console.log("API disponível através da urlhttp://localhost:8000")
 })
